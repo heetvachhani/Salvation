818ea22b3aa6f9512047d5d343886ad8acbc4687
Heet Vachhani <heet_vachhani@hotmail.com>
2018-04-21 22:08:44 -0500

    qrcode decode

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'))
+
 app.use('/', require('./routes/index'))
 
 connectToPeers(initialPeers);
